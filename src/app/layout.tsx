5e7cd0a68d45ee3d315da78ed6e0d906f970c521
htetahyan <htetahyan@gmail.com>
2024-03-03 07:39:37 -0500

    impl:(SEO) optimizing seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import localFont from "next/font/local";
 import {ReactNode} from "react";
 
 
+
 const NeueMontreal=localFont(
     {
         src: '../fonts/NeueMontreal-Bold.woff2',
@@ -21,9 +22,48 @@ const NeueMontrealRegular=localFont(
 )
 
 export const metadata: Metadata = {
-  title: 'Edusn',
-  description: 'Developer Portfolio',
-}
+  title: 'Htet Ah Yan',
+  description: 'An awesome web developer who loves to build web applications.',
+    robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+            follow: true,
+            index: true
+        }
+    },
+    icons: {
+        icon: '/favicon.ico',
+        apple: '/favicon.ico',
+        shortcut: '/favicon.ico'
+
+    },
+
+
+    openGraph: {
+        title: 'Htet Ah Yan',
+        description: 'An awesome web developer who loves to build web applications.',
+        url: 'https://htetahyan.vercel.app/',
+        releaseDate: '2023-03-02',
+
+
+
+        images: [
+            {
+                url: 'https://htetahyan.vercel.app/opengraph.png',
+                width: 1200,
+                height: 630
+            }
+        ]
+    },
+    appLinks: {
+        web: {
+            url: 'https://htetahyan.vercel.app',
+        }
+    },
+
+
+    }
 
 export default function RootLayout({
   children
@@ -31,16 +71,16 @@ export default function RootLayout({
   children:ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning  className={`${NeueMontreal.variable} ${NeueMontrealRegular.variable}  `}>
-      <body className={'bg-white'} >
-      <Providers >
+      <html lang="en" suppressHydrationWarning className={`${NeueMontreal.variable} ${NeueMontrealRegular.variable}  `}>
+
+      <body className={'bg-white'}>
+      <Providers>
 
 
-
-              {children}
+          {children}
 
 
       </Providers></body>
-    </html>
+      </html>
   )
 }
