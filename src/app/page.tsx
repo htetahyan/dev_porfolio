a6688784243eabc74ca514f04aed9e50c99f305d
HtetAhYan <htetahyan@gmail.com>
2024-03-05 15:51:32 +0630

    feat(add) json-d-schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 
 import React from 'react';
-import {Metadata} from "next";
-import metaTag from "@/components/MetaTag";
+
 import dynamic from "next/dynamic";
-import {JsonLd} from "@/utils/Json-ld";
+
 const Hero=dynamic(()=>import('@/components/sections/Hero/Hero'))
 const First=dynamic(()=>import('@/components/sections/Hero/First'))
 const Second=dynamic(()=>import('@/components/sections/Hero/second'))
@@ -12,10 +11,7 @@ const Third=dynamic(()=>import('@/components/sections/Hero/Third'))
 const Page = () => {
     return (
         <div className={''}>
-            <script
-                type="application/ld+json"
-                dangerouslySetInnerHTML={{ __html: JSON.stringify(JsonLd) }}
-            />
+
             <section className=" overflow-hidden h-screen w-full flex items-center relative  justify-center">
                 <Hero/>
 
